8651a847b527289c0d6389743fda6656bc0101a3
Michael Spiegel <michael.m.spiegel@gmail.com>
2019-06-25 17:00:26 -0400

    Apply pylint to bench.py script

--- bench.py
+++ bench.py
@@ -1,49 +1,56 @@
 #!/usr/bin/env python3
 
 import os
-import re
-import subprocess
-import sys
 
+import re
+
+import subprocess
 from subprocess import PIPE
 
-message_sizes = []
+import sys
+
+MESSAGE_SIZES = []
 
 for i in range(0, 21):
-    message_sizes.append(2 ** i)
+    MESSAGE_SIZES.append(2 ** i)
 
 if len(sys.argv) != 2:
     print("Usage: " + sys.argv[0] + " [ lat | thr ]")
     exit(1)
 
-suite = sys.argv[1]
+def main():
+    suite = sys.argv[1]
 
-if (suite != "lat") and (suite != "thr"):
-    print("Usage: " + sys.argv[0] + " [ lat | thr ]")
-    exit(1)
+    if (suite != "lat") and (suite != "thr"):
+        print("Usage: " + sys.argv[0] + " [ lat | thr ]")
+        exit(1)
 
-if suite == "thr":
-    message_count = 100_000
-    pattern = re.compile(r'average throughput: (\d+) Mb/s')
-else:
-    message_count = 100_000
-    pattern = re.compile(r'average latency: (\d+) ns')
+    if suite == "thr":
+        message_count = 1_000_000
+        pattern = re.compile(r'average throughput: (\d+) Mb/s')
+    else:
+        message_count = 100_000
+        pattern = re.compile(r'average latency: (\d+) ns')
 
-scriptdir = os.path.dirname(os.path.realpath(__file__))
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
 
-files = [f for f in os.listdir(scriptdir) if os.path.isfile(f)]
+    files = [f for f in os.listdir(scriptdir) if os.path.isfile(f)]
 
-for f in files:
-    if not os.access(f, os.X_OK):
-        continue
-    if not f.endswith(suite):
-        continue
-    scenario = f[:-len("_" + suite)]
-    if scenario == "tcp_local" or scenario == "tcp_remote":
-        continue
-    for message_size in message_sizes:
-        print(scenario, message_size, end =" ")
-        sys.stdout.flush()
-        completed = subprocess.run(["./" + f, str(message_size), str(message_count)], stdout=PIPE)
-        m = pattern.search(completed.stdout.decode('utf-8'))
-        print(m.group(1))
+    for filename in files:
+        if not os.access(filename, os.X_OK):
+            continue
+        if not filename.endswith(suite):
+            continue
+        scenario = filename[:-len("_" + suite)]
+        if scenario == "tcp_local" or scenario == "tcp_remote":
+            continue
+        for message_size in MESSAGE_SIZES:
+            print(scenario, message_size, end=" ")
+            sys.stdout.flush()
+            args = ["./" + filename, str(message_size), str(message_count)]
+            completed = subprocess.run(args, stdout=PIPE)
+            results = pattern.search(completed.stdout.decode('utf-8'))
+            print(results.group(1))
+
+if __name__ == "__main__":
+    main()
